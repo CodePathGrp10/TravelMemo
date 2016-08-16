0dd4765b626330466b1b595b13e9b1a2cfdce9d3
Akshat Jain <akshuu@gmail.com>
2016-08-15 23:53:17 -0700

    updating gradle version

--- app/src/main/java/com/grp10/codepath/travelmemo/activities/SplashScreenActivity.java
+++ app/src/main/java/com/grp10/codepath/travelmemo/activities/SplashScreenActivity.java
@@ -1,7 +1,9 @@
-package com.grp10.codepath.travelmemo;
+package com.grp10.codepath.travelmemo.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.grp10.codepath.travelmemo.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
