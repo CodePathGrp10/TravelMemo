3a35e4da5252fd1ac7679f73f7ce49da5c2b3992
abc <acb@aol.com>
2016-08-17 21:56:14 -0700

    debug firebase
    debug firebase

--- app/src/main/java/com/grp10/codepath/travelmemo/activities/SplashScreenActivity.java
+++ app/src/main/java/com/grp10/codepath/travelmemo/activities/SplashScreenActivity.java
@@ -5,25 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 
 import com.grp10.codepath.travelmemo.R;
-import com.mikepenz.google_material_typeface_library.GoogleMaterial;
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IProfile;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -40,7 +24,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainAct = new Intent(SplashScreenActivity.this,TripActivity.class);
+                Intent mainAct = new Intent(SplashScreenActivity.this,MainActivity.class);
                 mainAct.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(mainAct);
                 finish();
