97bb46ba9ea97b5c7d33c1a73894dfa731999842
abc <acb@aol.com>
2016-08-22 16:23:37 -0700

    Add capture activity
    Add capture activity

---
+++ app/src/main/java/com/grp10/codepath/travelmemo/activities/AddCaptureActivity.java
@@ -0,0 +1,35 @@
+package com.grp10.codepath.travelmemo.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.grp10.codepath.travelmemo.R;
+
+public class AddCaptureActivity extends AppCompatActivity implements AddCaptureActivityFragment.AddCaptureListener {
+    private String mTripId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_capture);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mTripId = getIntent().getStringExtra("tripId");
+
+        startAddCaptureFragment();
+    }
+
+    private void startAddCaptureFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        AddCaptureActivityFragment addCaptureActivityFragment = AddCaptureActivityFragment.newInstance(mTripId);
+        addCaptureActivityFragment.show(fm, "fragment_add_capture");
+    }
+
+    @Override
+    public void onFinishAddCapture() {
+        finish();
+    }
+}
