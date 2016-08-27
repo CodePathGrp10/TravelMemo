31310bed16cca18aedc76b36646009d7b0724c0d
abc <acb@aol.com>
2016-08-27 10:59:07 -0700

    Add kickflip key and secret
    Add kickflip key and secret

---
+++ app/src/main/java/com/grp10/codepath/travelmemo/kickflip/SECRETS.java
@@ -0,0 +1,9 @@
+package com.grp10.codepath.travelmemo.kickflip;
+
+/**
+ * Created by User on 8/27/2016.
+ */
+public class SECRETS {
+    public static final String CLIENT_KEY = "6XJX1Xw6z8=dtTDnaqKE4xLAht0GHSVikTDr-Ft1";
+    public static final String CLIENT_SECRET = "VK:h_yJkGS?1EQ=tNy7-OS1u4PBtPYvn3LQDxf2Mr2jvzFqC05ZodHvGDtg8HxVj@2XxYO46xLB?IX9KN?MD2ePg2_Axw75:BJPFLFcjQ0_maf2uGg0v=d3K@-p4FRem";
+}
