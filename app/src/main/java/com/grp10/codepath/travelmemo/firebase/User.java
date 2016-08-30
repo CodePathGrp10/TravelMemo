a2f6aae7fb6a1e44306e81eee1f4eedc9d92b8b6
abc <acb@aol.com>
2016-08-29 22:34:57 -0700

    User model: add emailId getter setter
    User model: add emailId getter setter

--- app/src/main/java/com/grp10/codepath/travelmemo/firebase/User.java
+++ app/src/main/java/com/grp10/codepath/travelmemo/firebase/User.java
@@ -50,6 +50,14 @@ public class User implements Parcelable {
         this.uid = uid;
     }
 
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("User{");
